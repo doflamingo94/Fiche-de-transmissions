a051c05b980c5e8e6e4d6628b0b655b03b97d264
doflamingo94 <55181148+doflamingo94@users.noreply.github.com>
2024-03-28 14:51:17 +0100

    Update Manager.php

--- model/Manager.php
+++ model/Manager.php
@@ -4,7 +4,7 @@ class Manager
 {
 	protected function dbConnect()
 	{
-		$db = new PDO('mysql:host=localhost;dbname=transmission;charset=utf8', 'c1635810c_khaled', 'Shikamaru16');
+		$db = new PDO('mysql:host=localhost;dbname=transmission;charset=utf8', 'c1635810c', 'testtest');
 		return $db;
 	}
 }
